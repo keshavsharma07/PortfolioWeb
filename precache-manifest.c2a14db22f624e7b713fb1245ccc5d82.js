a5c5b06eadb3ec137ca93bedcdc8d8784d653724
Keshav Sharma <keshavgalav07@gmail.com>
2020-09-29 14:58:44 +0530

    Updates

---
+++ precache-manifest.c2a14db22f624e7b713fb1245ccc5d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb1555591abdac3c0962351ad263fdf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8c9ed96f64856a231854",
+    "url": "/Portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "3f613d1bcbbbad5a529f",
+    "url": "/Portfolio/static/css/main.781f1f72.chunk.css"
+  },
+  {
+    "revision": "8c9ed96f64856a231854",
+    "url": "/Portfolio/static/js/2.cdfe1da5.chunk.js"
+  },
+  {
+    "revision": "7a9ffbcb0705b685a94df36082c8d5cd",
+    "url": "/Portfolio/static/js/2.cdfe1da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f613d1bcbbbad5a529f",
+    "url": "/Portfolio/static/js/main.883e7d34.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
